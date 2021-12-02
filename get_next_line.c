dc6a1a5b6a75e6fc322701decfe80dd29e4c7f1f
Vanessa Haefeli <vhaefeli@student.42lausanne.ch>
2021-12-02 22:48:26 +0100

    malloc a controller

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vhaefeli <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 10:01:08 by vhaefeli          #+#    #+#             */
-/*   Updated: 2021/12/02 18:59:41 by vhaefeli         ###   ########.fr       */
+/*   Updated: 2021/12/02 22:47:12 by vhaefeli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,9 @@ char	*ft_strcpyi(char *str, size_t i)
 	int		j;
 
 	j = 0;
-	cpy = malloc(strlen(str) - i);
+	cpy = malloc(ft_strlen(str) - i);
 	i++;
-	while (i < strlen(str))
+	while (i < ft_strlen(str))
 	{
 		cpy[j++] = str[i++];
 	}
@@ -76,7 +76,7 @@ char *ft_strcpy(char *src)
 	int i;
 
 	i = 0;
-	dst = malloc(strlen(src) + 1);
+	dst = malloc(ft_strlen(src) + 1);
 	while (src[i] != '\0')
 	{
 		dst[i] = src[i];
@@ -86,61 +86,64 @@ char *ft_strcpy(char *src)
 	return (dst);
 }
 
+char*	ft_char_ini(char *str)
+{
+	if (str == NULL)
+	{
+		str = malloc(1);
+		str[0] = '\0';
+	}
+	return (str);
+}
+
+char	*ft_fill_line(char *p1, char *p2, char *p3, size_t i)
+{
+	if (ft_strlen(p2) < ft_strlen(p3))
+	{	
+		p2 = ft_strjoini(p3, p1, i);
+		free(p3);
+		return (p2);
+	}
+	else
+	{
+		p3 = ft_strjoini(p2, p1, i);
+		free(p2);
+		return (p3);
+	}
+}
+
+int	ft_searchendline(char *p1)
+{
+	int i;
+
+	i = 0;
+	while (p1[i] != '\n' && p1[i] != '\0')
+		i++;
+	return (i);
+}
+
 char	*get_next_line(int fd)
 {
 	char		p1[BUFFER_SIZE + 1];
 	char		*p2;
 	static char	*p3 = NULL;
-	size_t		i;
-	int			a;
+	int		i;
 
 	i = 0;
-	a = 0;
-	if (p3 == NULL)
-	{
-		p3 = malloc(1);
-		p3[0] = '\0';
-	}
+	p3 = ft_char_ini(p3);
+	p2 = ft_char_ini(p3);
 	read(fd, p1, BUFFER_SIZE);
 	if (p1[1] == '\0')
 		return (NULL);
-	p1[BUFFER_SIZE] = '\0';
 	while (i >= 0)
 	{
-		while (p1[i] != '\n' && p1[i] != '\0')
-		{
-			i++;
-		}
-		if (a == 0)
-		{
-			p2 = ft_strjoini(p3, p1, i);
-			free(p3);
-			a = 1;
-		}
-		else
-		{
-			p3 = ft_strjoini(p2, p1, i);
-			free(p2);
-			a = 0;
-		}
+		i = ft_searchendline(p1);
+		p2 = ft_fill_line(p1, p2, p3, i);
 	 	if (i == BUFFER_SIZE)
-		{
-			i = 0;
 			read(fd, p1, BUFFER_SIZE);
-		}
 		else
 		{
-			if (a == 1)
-			{
-				p3 = ft_strcpyi(p1, i);
-				a = 0;
-			}
-			else
-			{
-				p2 = ft_strcpy(p3);
-			//	free(p3);
-				p3 = ft_strcpyi(p1, i);
-			}
+			p3 = ft_strcpyi(p1, i);
 			i = -1;
 		}
 	}
